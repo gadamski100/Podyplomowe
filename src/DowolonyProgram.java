13d666f92fa6458a23fb19fbe930b1fa64da3065
gadamski <gadamski@student.wszib.edu.pl>
2023-10-08 16:04:23 +0200

    Initial commit

---
+++ src/DowolonyProgram.java
@@ -0,0 +1,12 @@
+public class DowolonyProgram {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 10.5F;
+        long c = 951801001811L;
+
+        System.out.println("To jest Dowolny Program, który na podstawie trzech liczb jakimi są:");
+        System.out.println(a + ", " + b + " oraz " + c);
+        System.out.println("wykona dowolne obliczenie matematyczne jakie sobie tylko sam zaprogramujesz.");
+
+    }
+}
